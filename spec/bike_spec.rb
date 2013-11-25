a0eaebfdc6b01ec6cd29e7fda0ac44eda567761e
Gianni Guitteaud <gianni.guitteaud@gmail.com>
2013-11-25 17:16:15 +0000

    Added DockingStation files to the project

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,25 +4,25 @@ require 'bike'
 
 describe Bike do 
 	
+	let(:bike) {Bike.new} 
+#is the same as writting bike = Bike.new eache time
+#so that in my specify, i dont repeat it
 	specify "it should not be broken after we create it" do
-	bike = Bike.new 
 	#new object in Bike class
 	expect(bike).not_to be_broken
 	#expect an instance of this class
 	#a method "broken?" that should return false
 	end
 	
-	specify "it should be able to break" do
-	bike = Bike.new 
+	specify "it should be able to break" do 
 	bike.break
 	expect(bike).to be_broken
 	end
 
 	specify "it should be able to get fixed" do
-		bike = Bike.new
-		bike.break
-		bike.fix
-		expect(bike).not_to be_broken
+	bike.break
+	bike.fix
+	expect(bike).not_to be_broken
 	end
 end
 
