9a144585e1e195567284f4ec51085584a6cc2dd7
Gianni Guitteaud <gianni.guitteaud@gmail.com>
2013-11-26 12:10:30 +0000

    Added Availability feature

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -24,4 +24,8 @@ class DockingStation
 	def full?
 		bike_count == @capacity
 	end	
+
+	def available_bikes
+		@bikes.reject {|bike| bike.broken?}
+	end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -28,9 +28,18 @@ let(:station) {DockingStation.new(:capacity => 20)}
   	expect(station).to be_full
   end
 
-  specify " it should not accept a bike if it's full" do
+  specify "it should not accept a bike if it's full" do
   fill_station(station)
   expect(lambda {station.dock(bike)}).to raise_error(RuntimeError)
   end
 
+specify "it should provide the list of available bikes" do
+  working_bike, broken_bike = Bike.new, Bike.new    
+  broken_bike.break
+  station.dock(working_bike)
+  station.dock(broken_bike)
+  expect(station.available_bikes).to eq([working_bike])
+  end
+
+  
 end
